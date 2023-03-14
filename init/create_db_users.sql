f799544727b2c210c2b8e4f8484b1a0da20bb074
Daan <72937910+DaanG4@users.noreply.github.com>
2023-03-14 10:53:37 +0100

    Delete init directory

--- init/create_db_users.sql
+++
@@ -1,9 +0,0 @@
-# query to create user
-
-CREATE TABLE users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
